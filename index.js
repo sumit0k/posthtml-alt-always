9193e65dff1dce1825b5c9d442861a817e6b997f
Sumit <7878332+sumit0k@users.noreply.github.com>
2025-02-05 15:04:34 +0530

    a better way to match multiple tags

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const filename = function (filePath) {return filePath.split('/').pop().split('.'
 
 module.exports = function () {
 	return function altAlways(tree) {
-		tree.match({ tag: 'img', attrs: { alt: false } }, function(node) {
+		tree.match([{ tag: 'img', attrs: { alt: '' } },{ tag: 'img', attrs: { alt: false } }], function(node) {
             node.attrs.alt = filename(node.attrs.src);
-			return node;
-		});
-        tree.match({ tag: 'img', attrs: { alt: '' } }, function(node) {
-			node.attrs.alt = filename(node.attrs.src);
 			return node;
 		});
 	};
